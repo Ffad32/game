2875f0fa11f247a7c4f24459143ab0305d1b3d7a
Ffad32 <134131966+Ffad32@users.noreply.github.com>
2024-04-25 12:33:38 +0200

    Update scenegen.h

--- scenegen.h
+++ scenegen.h
@@ -4,7 +4,7 @@ void processNPCSquare(int suffix, char* text,int scenemun) {
     
     switch (suffix) {
         case 1:
-
+            strcpy(text, "maps/map1.map");
             break;
         case 2:
             break;
